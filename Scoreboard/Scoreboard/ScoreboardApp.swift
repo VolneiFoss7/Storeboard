34c264ea68dd2bac858a7aaa3728daf026cee78b
Volnei Foss <volnei.foss@townsq.com.br>
2025-11-07 18:33:29 -0300

    inital project

---
+++ Scoreboard/Scoreboard/ScoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreboardApp.swift
+//  Scoreboard
+//
+//  Created by Volnei Foss on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
